1ebc46a237314e90cec0f0cd2d6aab80fc21faf8
George Kirby <George-Kirby@live.co.uk>
2022-01-18 21:44:54 +0000

    Added NameChange WOM call

--- CorruptOSBot/Modules/RSNModule.cs
+++ CorruptOSBot/Modules/RSNModule.cs
@@ -143,7 +143,7 @@ namespace CorruptOSBot.Modules
                 string.Format("{0} has changed their name to <@{1}> {2}", previousName, ((SocketGuildUser)currentUser).Id, (forced ? "(Forced by staff)" : string.Empty))));
 
             // update WOM
-            //new WiseOldManClient().PostNameChange(previousName, preferedNickname);
+            new WiseOldManClient().PostNameChange(previousName, preferedNickname);
 
             try
             {
